75b4fe9ea193a6a6f673fff315560513f3843186
Jason Yoon <yoonjesung@users.noreply.github.com>
2019-12-24 21:20:10 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+packages = ['opentoken']
+
+with open("requirements.txt", "r") as f:
+    requires = f.read().split("\n")
+
+test_requirements = [
+    "pytest==5.3.2"
+]
+
+setuptools.setup(
+    name="opentoken",
+    version="1.0.0",
+    description="OpenToken support for python.",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author="Jason Yoon",
+    url="https://github.com/yoonjesung/opentoken-python",
+    packages=packages,
+    package_dir={"opentoken": "opentoken"},
+    python_requires=">=3.5",
+    install_requires=requires,
+    license="MIT",
+    tests_require=test_requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)

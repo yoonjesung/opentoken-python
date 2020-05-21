f49df1f59000cd5a17d1f41cbdcf7fcfc230ea94
Jason Yoon <yoonjesung@users.noreply.github.com>
2020-05-21 11:58:16 -0400

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setuptools.setup(
     name="opentoken",
-    version="1.0.1",
+    version="1.1.0",
     description="OpenToken support for python.",
     long_description=readme,
     long_description_content_type='text/markdown',
